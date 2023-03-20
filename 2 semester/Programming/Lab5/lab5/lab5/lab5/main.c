56b5694c03d600e180f750265bd3c5dea0004088
Dynchikkk <dandombra1@gmail.com>
2023-03-20 20:51:17 +0300

    прога

--- 2 semester/Programming/Lab5/lab5/lab5/lab5/main.c
+++ 2 semester/Programming/Lab5/lab5/lab5/lab5/main.c
@@ -48,6 +48,7 @@ int main()
         pop e;
 #pragma endregion
 
+        call CALCULATION;
         call TO_ASCII;
 
         // Выход из программы 
@@ -64,7 +65,7 @@ int main()
     FROM_ASCII_LOOP:
         movzx edx, [eax + ecx - 1]; // конвертируем символ из ASCII в нормальный вид
         push eax; // кладем ссылку на строку, чтобы освободить регистр
-        mov eax, 45;  // кладем в eax "-" в нормальном виде =>
+        mov eax, '-';  // кладем в eax "-" в нормальном виде =>
         cmp edx, eax; // и сравниваем, не является ли текущий символ "-"
         je MINUS_F; // если да то преходим по метке MINUS
         jmp PLUS_F; // если нет, то переходим по метке PLUS
@@ -73,7 +74,6 @@ int main()
         jmp END_FA_LOOP;
     PLUS_F:
         sub edx, 48; // вычитаем 48, чтобы полчить число в привычном виде
-        cdq;
         imul edx, razr; // умножаем число на разряд
         add ebx, edx; // приплюсовываем число к итоговому
     END_FA_LOOP:
@@ -88,17 +88,77 @@ int main()
         mov razr, 1;
         mov znak, 1;
         pop ebp;
-        /*ret;*/
+        ret;
 
         // ------------------- //
+    CALCULATION:
+        push ebp;
+        mov ebp, esp;
+        mov eax, aInt;
+        mov ebx, bInt;
+        cdq;
+        idiv ebx;
+        push ebx;
+        mov eax, cInt;
+        mov ebx, dInt;
+        imul eax, ebx;
+        pop ebx;
+        add eax, ebx;
+        sub eax, eInt;
+        mov yInt, eax;
+        pop ebp;
+        ret;
+
+        // ------------------- //
+    TO_ASCII:
+        push ebp;
+        mov ebp, esp;
+        mov eax, yInt;
+        mov ecx, NUM_SIZE * 2;
+    DIVIDE:
+        cmp eax, 0; // смотрим, отрицательное ли число или нет
+        jl MINUS_T; // если да, то перезодим на метку MINUS_T
+    PLUS_T:
+        cdq;
+        mov ebx, 10; // делим число на 10, 
+        idiv ebx;    // чтобы получить отдельную цифру 
+        add edx, 48;
+        push edx;     // заносим 16 младших бит в стек
+        jmp END_TA_LOOP;
+    MINUS_T:
+        mov znak, -1;
+        mov ebx, -1; // меняем знак числа
+        imul ebx;    // 
+    END_TA_LOOP:
+        cmp eax, 0;       // смотрим не закончилось ли число
+        je END_DIVIDE;    // если закончилось то выходим из цикла
+        loop DIVIDE;
+    END_DIVIDE:
+        mov ebx, NUM_SIZE * 2 + 1; // считаем счетчик 
+        sub ebx, ecx;              //
+        mov ecx, ebx;              //
+        lea esi, [y]; // кладем в eax ссылку на начало y
+        cmp znak, -1;      // смотрим не отрицательное ли число
+        jne TO_ASCII_LOOP; // если да 
+        mov[esi], '-'; // то в начало y кладем -
+        inc esi; // 
+        dec ecx; // 
+    TO_ASCII_LOOP:
+        pop ebx; // берем остаток числ из стека
+        mov[esi], ebx; // кладем его в y
+        inc esi; // 
+        loop TO_ASCII_LOOP; 
+        mov znak, 1;
+        pop ebp;
+        ret;
     
 #pragma endregion
-
+        
     EXIT:
     }
 
-    printf("%d, %d, %d, %d, %d", aInt, bInt, cInt, dInt, eInt);
-    printf("\n%s", y);
+    printf("Ishod dannye: %d, %d, %d, %d, %d\n", aInt, bInt, cInt, dInt, eInt);
+    printf("Result: y = a/b+c*d-e = %s", y);
 
 	return 0;
 }
