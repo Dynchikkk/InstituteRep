4222e539c162f912cc04364a4077212e3a1e85be
Daniil <dandombra1@gmail.com>
2024-09-29 18:44:08 +0300

    [TC]: lab3 code

---
+++ semester_5/Translyatory_and_Compilyatory/lab3/code/binTree.h
@@ -0,0 +1,26 @@
+#ifndef BIN_TREE
+#define BIN_TREE
+
+typedef struct Node {
+	int value;
+	struct Node* left;
+	struct Node* right;
+} Tree;
+
+Tree* CreateTree(int val);
+
+Tree* AddNode(int val, Tree* root);
+
+Tree* SearchNode(int val, Tree* root);
+
+int EntryCount(int value, Tree* root, int entry);
+
+void TreePrint(Tree* root, int tabs);
+
+void ClearTree(Tree* root);
+
+void PrintOneNode(Tree* root);
+
+void TreeWalk(Tree* root);
+
+#endif 
