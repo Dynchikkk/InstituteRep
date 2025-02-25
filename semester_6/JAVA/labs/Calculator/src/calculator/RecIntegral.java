ad2912e222320f80125afe419fd09768c346ffdd
dandombra1 <dandombra1@gmail.com>
2025-02-25 19:51:12 +0300

    [JAVA]: lab2

--- semester_6/JAVA/labs/Calculator/src/calculator/RecIntegral.java
+++ semester_6/JAVA/labs/Calculator/src/calculator/RecIntegral.java
@@ -5,56 +5,46 @@ package calculator;
  * @author admin
  */
 public class RecIntegral {
-    public int Id;
-    
-    private boolean _isCalculated = false;
     private double _topBorder = 0.0;
     private double _bottomBorder = 0.0;
     private double _stepWidth = 0.0;
     private double _result = 0.0;
     
-    public RecIntegral(double topBorder, double bottomBorder, double stepWidth, int id) {
+    public RecIntegral(double topBorder, double bottomBorder, double stepWidth) {
         _topBorder = topBorder;
         _bottomBorder = bottomBorder;
         _stepWidth = stepWidth;
-        Id = id;
+        _result = Double.NaN;
     }
     
-    public boolean isCalculated() { return _isCalculated; }
     public double getTopBorder() { return _topBorder; }
     public double getBottomBorder() { return _bottomBorder; }
     public double getStepWidth() { return _stepWidth; }
-    public double getResult() {
-        if (!_isCalculated){
-            _result = calculateIntegral(_topBorder, _bottomBorder, _stepWidth);
-            _isCalculated = true;
-        }
-        return _result;
-    }
+    public double getResult() { return _result; }
     
-    private double calculateIntegral(double topBorder, double bottomBorder, double stepWidth) {
+    public void calculateIntegral() {
         // Ensure that the step width is positive
-        if (stepWidth <= 0) {
+        if (_stepWidth <= 0) {
             throw new IllegalArgumentException("Step width must be positive");
         }
 
         double sign = 1.0;
         // If the top border is less than the bottom border, swap them and invert the sign of the result
-        if (topBorder < bottomBorder) {
-            double temp = topBorder;
-            topBorder = bottomBorder;
-            bottomBorder = temp;
+        if (_topBorder < _bottomBorder) {
+            double temp = _topBorder;
+            _topBorder = _bottomBorder;
+            _bottomBorder = temp;
             sign = -1.0;
         }
 
         double sum = 0.0;
         // Integrate using the trapezoidal rule
-        for (double x = bottomBorder; x < topBorder; x += stepWidth) {
-            double nextX = Math.min(x + stepWidth, topBorder);
+        for (double x = _bottomBorder; x < _topBorder; x += _stepWidth) {
+            double nextX = Math.min(x + _stepWidth, _topBorder);
             // Calculate the area of the trapezoid between x and nextX
             double area = (nextX - x) * (Math.exp(-x) + Math.exp(-nextX)) / 2.0;
             sum += area;
         }
-        return sign * sum;
+        _result =  sign * sum;
     }
 }
