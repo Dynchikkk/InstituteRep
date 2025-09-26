52e239e5b455a020b1fd00e503002488f0320d26
Daniil Belyaev <dandombra1@gmail.com>
2025-09-26 22:08:04 +0300

    [PVS]: lab1

--- semester_7/Proektirovanie_vstrayvaemyh_sredstv/labs/lab1/task1.asm
+++ semester_7/Proektirovanie_vstrayvaemyh_sredstv/labs/lab1/task1.asm
@@ -1,4 +1,4 @@
-    list        p=16f887        ; указать целевой микроконтроллер
+    list        p=16f887        ; целевой микроконтроллер
     #include    <p16f887.inc>   ; определения регистров и констант для PIC16F887
 
     __CONFIG    _CONFIG1, _LVP_OFF&_FCMEN_OFF&_IESO_OFF&_BOR_ON&_CPD_OFF&_CP_OFF&_MCLRE_ON&_PWRTE_ON&_WDT_ON&_INTRC_OSC_NOCLKOUT
@@ -27,37 +27,37 @@ INT_VECTOR      CODE    0x0004 ; вектор прерывания
     movf    PCLATH,w          ; загрузить PCLATH в W
     movwf   pclath_temp       ; сохранить PCLATH
 
-; здесь должен быть код ISR (если есть). В вашем фрагменте ISR пустой.
-
     movf    pclath_temp,w     ; восстановить PCLATH из pclath_temp
     movwf   PCLATH
     movf    status_temp,w     ; восстановить STATUS
     movwf   STATUS
-    swapf   w_temp,f          ; !!! НЕСТАНДАРТНЫЙ приём: меняет местами стар/млад нибблы w_temp и сохраняет в w_temp
-    swapf   w_temp,w          ; !!! затем помещает результат в W (восстанавливает W только частично/необычно)
+    swapf   w_temp,f          ; меняет местами стар/млад нибблы w_temp и сохраняет в w_temp
+    swapf   w_temp,w          ; затем помещает результат в W (восстанавливает W только частично/необычно)
     retfie                    ; возврат из прерывания (восстанавливает GIE)
 
 MAIN_PROG       CODE
 
 start
-
-  movlw 0x00
-  banksel TRISD               ; выбрать банк, содержащий TRISD
-  movwf TRISD                 ; TRISD = 0x00 -> порт D как выходы
-  movlw 0x55
-  banksel PORTD               ; выбрать банк PORTD
-  movwf PORTD                 ; записать 0x55 (01010101) в PORTD — начальное состояние портD
+	movlw 0x00
+	banksel TRISD               ; выбрать банк, содержащий TRISD
+	movwf TRISD                 ; TRISD = 0x00 -> порт D как выходы
+	
+	movlw 0x55
+	banksel PORTD               ; выбрать банк PORTD
+	movwf PORTD                 ; записать 0x55 (01010101) в PORTD — начальное состояние портD
 
 maincycle
-  CLRWDT                     ; очистить сторожевой таймер (WDT)
+    CLRWDT                     ; очистить сторожевой таймер (WDT)
 pause
-  decfsz count1,1            ; декремент младшего байта count1; если 0->skip следующую строчку
-  goto pause                 ; если не равен нулю — возвращаемся в pause
-  decfsz count1+1,1          ; декремент старшего байта count1+1; если не ноль — goto pause
-  goto pause                 ; (эта строка: появление — в коде; логика: при старшем не-нуле goto pause)
+    decfsz count1,1            ; декремент младшего байта count1; если 0->skip следующую строчку
+    goto pause                 ; если не равен нулю — возвращаемся в pause
+    decfsz count1+1,1          ; декремент старшего байта count1+1; если не ноль — goto pause
+    goto pause                 ; (эта строка: появление — в коде; логика: при старшем не-нуле goto pause)
+	
 ; Сдвиг содержимого регистра влево без учета бита переноса
-  rlf PORTD,0                ; rotate left PORTD, результат записан обратно в PORTD (d = 0)
-  rlf PORTD,1                ; rotate left PORTD, результат записан в W (d = 1) — файл не изменится
-  goto maincycle             ; бесконечный цикл
+    rlf PORTD,0                ; rotate left PORTD, результат записан обратно в PORTD (d = 0)
+    rlf PORTD,1                ; rotate left PORTD, результат записан в W (d = 1) — файл не изменится
+	
+    goto maincycle             ; бесконечный цикл
 
   END
--- semester_7/Proektirovanie_vstrayvaemyh_sredstv/labs/lab1/task3.asm
+++ semester_7/Proektirovanie_vstrayvaemyh_sredstv/labs/lab1/task3.asm
@@ -4,79 +4,66 @@
     __CONFIG    _CONFIG1, _LVP_OFF&_FCMEN_OFF&_IESO_OFF&_BOR_ON&_CPD_OFF&_CP_OFF&_MCLRE_ON&_PWRTE_ON&_WDT_ON&_INTRC_OSC_NOCLKOUT
     __CONFIG    _CONFIG2, _WRT_OFF & _BOR21V
 
+; Обычный сегмент данных
 TEMP_VAR    UDATA
-count1      RES 2
+count1      RES 2    ; переменная для задержки
 
-RESET_VECTOR CODE 0x0000
-    movlw HIGH start
-    movwf PCLATH
-    goto start
+;**********************************************************************
+RESET_VECTOR    CODE    0x0000
+    movlw   HIGH start
+    movwf   PCLATH
+    goto    start
 
-MAIN_PROG CODE
+MAIN_PROG       CODE
 
 start
-    ; --- Отключение аналогов (чтобы RB0 можно было использовать как цифровой)
-    movlw 0x00
-    banksel ANSEL
-    movwf ANSEL           ; отключаем AN0..AN7 (0 = цифровой)
-    banksel ANSELH
-    movwf ANSELH          ; отключаем AN8..AN12 и т.д.
-
-    ; --- Настройка портов
-    movlw 0x00
+    ; Инициализация портов
     banksel TRISD
-    movwf TRISD           ; PORTD как выходы
-
-    movlw 0x01
-    banksel TRISB
-    movwf TRISB           ; сделать весь PORTB входами (при необходимости)
-                          ; далее мы будем проверять только RB0 — это TRISB<0>=1
-
-    ; --- Инициализация начальных значений
-    movlw 0x11
+    movlw   0x00        ; PORTD - выходы
+    movwf   TRISD
+    movlw   0x01        ; RB0 - вход, остальные - выходы
+    movwf   TRISB
+    banksel ANSELH
+    clrf    ANSELH      ; отключаем аналоговые функции
     banksel PORTD
-    movwf PORTD           ; начальное состояние PORTD = 0x11
+    clrf    PORTD       ; очищаем PORTD
 
-    ; инициализация count1 задержки (пример)
-    movlw 0x20
-    movwf count1          ; младший байт задержки
-    movlw 0x00
-    movwf count1+1        ; старший байт
+main_loop
+    btfsc   PORTB, 0    ; проверяем состояние кнопки на RB0
+    goto    mode2       ; если нажата - режим 2
+    goto    mode1       ; если не нажата - режим 1
 
-maincycle
-    CLRWDT
+mode1
+    movlw   0x55        ; начальное значение для режима 1
+    movwf   PORTD
+mode1_loop
+    call    delay       ; задержка
+    rlf     PORTD, F    ; сдвиг влево (через перенос)
+    btfss   PORTB, 0    ; проверяем кнопку
+    goto    mode1_loop  ; продолжаем если не нажата
+    goto    main_loop   ; возвращаемся к проверке
 
-    ; Чтение состояния RB0
-    banksel PORTB
-    btfsc PORTB,0         ; если RB0 == 1 (bit set), пропустить следующую линию
-    goto button_pressed   ; RB0 = 1 -> кнопка нажата
-    goto button_released  ; RB0 = 0 -> кнопка не нажата
+mode2
+    movlw   0x11        ; начальное значение для режима 2
+    movwf   PORTD
+mode2_loop
+    call    delay       ; задержка
+    rlf     PORTD, F    ; сдвиг влево (через перенос)
+    btfsc   PORTB, 0    ; проверяем кнопку
+    goto    mode2_loop  ; продолжаем если нажата
+    goto    main_loop   ; возвращаемся к проверке
 
-button_released
-    ; поведение, когда кнопка НЕ нажата — например стандартный сдвиг влево
-    banksel PORTD
-    rlf PORTD,0           ; сдвиг влево, результат в PORTD
-    call delay_simple
-    goto maincycle
-
-button_pressed
-    ; поведение, когда кнопка нажата — альтернативное (например инверсия + мигание)
-    banksel PORTD
-    comf PORTD,0          ; инвертировать PORTD и записать обратно
-    call delay_simple
-    goto maincycle
-
-; простая задержка: декремент двухбайтового count1
-delay_simple
-    movlw 0x50
-    movwf count1
-    movlw 0x00
-    movwf count1+1
-d_loop
-    decfsz count1,1
-    goto d_loop
-    decfsz count1+1,1
-    goto d_loop
+; Подпрограмма задержки
+delay
+    movlw   0xFF        ; инициализируем счетчик задержки
+    movwf   count1
+    movlw   0xFF
+    movwf   count1+1
+delay_loop
+    decfsz  count1, F
+    goto    delay_loop
+    decfsz  count1+1, F
+    goto    delay_loop
     return
 
     END
