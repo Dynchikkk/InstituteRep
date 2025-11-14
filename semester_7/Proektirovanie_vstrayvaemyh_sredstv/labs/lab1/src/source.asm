7e693f3c56139f3b29bad7810ee47f651cdcdae2
Daniil Belyaev <dandombra1@gmail.com>
2025-11-15 01:58:32 +0300

    [PVS] + [PPOMS]: labs

--- semester_7/Proektirovanie_vstrayvaemyh_sredstv/labs/lab1/src/source.asm
+++ semester_7/Proektirovanie_vstrayvaemyh_sredstv/labs/lab1/src/source.asm
@@ -1,69 +1,48 @@
-    list        p=16f887
-    #include    <p16f887.inc>
-
+    list        p=16f887    ; list directive to define processor
+    #include    <p16f887.inc>   ; processor specific variable definitions
     __CONFIG    _CONFIG1, _LVP_OFF&_FCMEN_OFF&_IESO_OFF&_BOR_ON&_CPD_OFF&_CP_OFF&_MCLRE_ON&_PWRTE_ON&_WDT_ON&_INTRC_OSC_NOCLKOUT
     __CONFIG    _CONFIG2, _WRT_OFF & _BOR21V
-
 ; Обычный сегмент данных
-TEMP_VAR    UDATA
-count1      RES 2    ; переменная для задержки
-
+TEMP_VAR    UDATA           ; explicit address specified is not required
+count1      RES 2
 ;**********************************************************************
-RESET_VECTOR    CODE    0x0000
-    movlw   HIGH start
-    movwf   PCLATH
-    goto    start
+RESET_VECTOR    CODE    0x0000 ; processor reset vector
+    movlw HIGH start
+    movwf PCLATH
+    goto start              ; go to beginning of program
 
-MAIN_PROG       CODE
+INT_VECTOR      CODE    0x0004 ; interrupt vector location
+МАIN_PROG    CODE
+start    ; начало программы
 
-start
-    ; Инициализация портов
-    banksel TRISD
-    movlw   0x00        ; PORTD - выходы
-    movwf   TRISD
-    movlw   0x01        ; RB0 - вход, остальные - выходы
-    movwf   TRISB
-    banksel ANSELH
-    clrf    ANSELH      ; отключаем аналоговые функции
-    banksel PORTD
-    clrf    PORTD       ; очищаем PORTD
+movlw 0x00    ; пересылка константы в регистр w (буфер)
+banksel TRISD    ; выбор страницы памяти
+movwf TRISD    ; занесение в TRISD значения из регистра w (0)
+movlw 0xff    ; пересылка 0xff в регистр w (буфер)
+movwf TRISB    ; занесение в TRISD значения из регистра w (0xff)
+banksel ANSELH   ; выбор страницы памяти
+movlw 0x3b    ; пересылка константы в регистр w (буфер)
+movwf ANSELH    ; занесение в ANSELH значения из регистра w (0xff)
+banksel PORTD    ; выбор страницы памяти
+movwf PORTD    ; занесение в PORTD значения из регистра w (0x3d)
 
-main_loop
-    btfsc   PORTB, 0    ; проверяем состояние кнопки на RB0
-    goto    mode2       ; если нажата - режим 2
-    goto    mode1       ; если не нажата - режим 1
+ml
+bsf PORTD,1    ; установка в PORTD в 1 бит "1"
+bsf PORTD,2    ; установка в PORTD в 2 бит "1"
+bcf PORTD,3    ; очистка 3 бита в PORTD
+CLRWDT    ; сброс ждущего таймера
 
-mode1
-    movlw   0x55        ; начальное значение для режима 1
-    movwf   PORTD
-mode1_loop
-    call    delay       ; задержка
-    rlf     PORTD, F    ; сдвиг влево (через перенос)
-    btfss   PORTB, 0    ; проверяем кнопку
-    goto    mode1_loop  ; продолжаем если не нажата
-    goto    main_loop   ; возвращаемся к проверке
+btfsc PORTB,1    ; проверка 1 бита в PORTB, пропуск следующей команды, если там 0
+goto ml    ; переход в метку ml
 
-mode2
-    movlw   0x11        ; начальное значение для режима 2
-    movwf   PORTD
-mode2_loop
-    call    delay       ; задержка
-    rlf     PORTD, F    ; сдвиг влево (через перенос)
-    btfsc   PORTB, 0    ; проверяем кнопку
-    goto    mode2_loop  ; продолжаем если нажата
-    goto    main_loop   ; возвращаемся к проверке
+m2
+bcf PORTD,1    ; очистка 1 бита в PORTD
+bcf PORTD,2    ; очистка 2 бита в PORTD
+bsf PORTD,3    ; установка в PORTD в 3 бит "1"
 
-; Подпрограмма задержки
-delay
-    movlw   0xFF        ; инициализируем счетчик задержки
-    movwf   count1
-    movlw   0xFF
-    movwf   count1+1
-delay_loop
-    decfsz  count1, F
-    goto    delay_loop
-    decfsz  count1+1, F
-    goto    delay_loop
-    return
+btfsc PORTB,1    ; проверка 1 бита в PORTB, пропуск следующей команды, если там 0
+goto ml    ; переход в метку ml
+goto m2    ; переход в метку m2
+end
 
-    END
+
